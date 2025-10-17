329cb65aa1020999b1aa7fbd886575bf0a48237d
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-10-17 10:38:46 +0200

    feat: add isSameLength, isValueEqualsIndex

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -139,8 +139,8 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((value) => value.length === arr[0].length);
 }
 
 /**
@@ -154,8 +154,8 @@ function isSameLength(/* arr */) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((value, i) => value === i);
 }
 
 /**
